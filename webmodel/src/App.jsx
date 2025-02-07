a5fc4049880bbd433c2076912021b5be9a82f918
Abdelah Nesedin Nasir <162048442+Abdunesr@users.noreply.github.com>
2025-02-07 12:02:18 +0300

    Update App.jsx

--- webmodel/src/App.jsx
+++ webmodel/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://machninelearning-v2-1.onrender.com/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
